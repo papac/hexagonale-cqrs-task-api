12c9031613c6de02544e6dfab922e65943232629
Franck DAKIA <dakiafranck@gmail.com>
2022-02-11 10:57:15 +0100

    Add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'app-cqrs',
+      script: './dist/src/main.js',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 8088,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 8088,
+      },
+    },
+  ],
+};
